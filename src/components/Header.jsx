d531b4b756175563a2cb66ac433d522b3b1af71d
Krushangpatel2610 <krushang2610.edu@gmail.com>
2024-12-04 16:47:19 +0530

    2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
-      <h1>React Art</h1>
+      <h1>React Js Art</h1>
       <p>A community of artists and art-lovers.</p>
     </header>
   );
